c7bab6062c503535ff60eaf9c38dc5a15725717f
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-11-28 10:35:46 -0500

    Better tail

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/start', (request, response) => {
   const data = {
     color: '#78866b',
     headType: 'fang',
-    tailType: 'sharp',
+    tailType: 'hook',
   };
 
   return response.json(data);
