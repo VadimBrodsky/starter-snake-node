f21c300c38cfd87b5b039cd0de2e6c6bc3e741cc
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-11-28 10:33:00 -0500

    Customize snake, prettier

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const logger = require('morgan')
-const app = express()
+const bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const app = express();
 const {
   fallbackHandler,
   notFoundHandler,
   genericErrorHandler,
-  poweredByHandler
-} = require('./handlers.js')
+  poweredByHandler,
+} = require('./handlers.js');
 
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
-app.set('port', (process.env.PORT || 9001))
+app.set('port', process.env.PORT || 9001);
 
-app.enable('verbose errors')
+app.enable('verbose errors');
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(poweredByHandler)
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(poweredByHandler);
 
 // --- SNAKE LOGIC GOES BELOW THIS LINE ---
 
@@ -27,11 +27,13 @@ app.post('/start', (request, response) => {
 
   // Response data
   const data = {
-    color: '#DFFF00',
-  }
+    color: '#78866b',
+    headType: 'fang',
+    tailType: 'sharp',
+  };
 
-  return response.json(data)
-})
+  return response.json(data);
+});
 
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
@@ -40,27 +42,27 @@ app.post('/move', (request, response) => {
   // Response data
   const data = {
     move: 'up', // one of: ['up','down','left','right']
-  }
+  };
 
-  return response.json(data)
-})
+  return response.json(data);
+});
 
 app.post('/end', (request, response) => {
   // NOTE: Any cleanup when a game is complete.
-  return response.json({})
-})
+  return response.json({});
+});
 
 app.post('/ping', (request, response) => {
   // Used for checking if this snake is still alive.
   return response.json({});
-})
+});
 
 // --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
-app.use('*', fallbackHandler)
-app.use(notFoundHandler)
-app.use(genericErrorHandler)
+app.use('*', fallbackHandler);
+app.use(notFoundHandler);
+app.use(genericErrorHandler);
 
 app.listen(app.get('port'), () => {
-  console.log('Server listening on port %s', app.get('port'))
-})
+  console.log('Server listening on port %s', app.get('port'));
+});
